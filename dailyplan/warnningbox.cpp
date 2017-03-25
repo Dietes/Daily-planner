d68c3745f26ba77a0bb52d6012be237aeefb7310
Dietes <dietes@foxmail.com>
2017-03-25 14:45:28 +0800

    unused

---
+++ dailyplan/warnningbox.cpp
@@ -0,0 +1,31 @@
+﻿#include "warnningbox.h"
+#include "ui_warnningbox.h"
+
+warnningbox::warnningbox(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::warnningbox)
+{
+    ui->setupUi(this);
+    setWindowFlags(Qt::FramelessWindowHint);
+    setFixedSize(360,170);
+}
+
+warnningbox::~warnningbox()
+{
+    delete ui;
+}
+
+
+
+void warnningbox::on_okbtn_clicked()
+{
+    MainWindow w;
+    w.saveFile();
+}
+
+
+
+bool warnningbox::on_celbtn_clicked()
+{
+    return true;
+}
---
+++ dailyplan/warnningbox.h
@@ -0,0 +1,29 @@
+﻿#ifndef WARNNINGBOX_H
+#define WARNNINGBOX_H
+
+#include <QDialog>
+#include <mainwindow.h>
+
+namespace Ui {
+class warnningbox;
+}
+
+class warnningbox : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit warnningbox(QWidget *parent = 0);
+    ~warnningbox();
+    void warnbox();
+
+private slots:
+    void on_okbtn_clicked();
+
+    bool on_celbtn_clicked();
+
+private:
+    Ui::warnningbox *ui;
+};
+
+#endif // WARNNINGBOX_H
